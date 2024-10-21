22a248e936c14a63adbb6a864087a5acab0e33fc
mohnish2908 <152661022+mohnish2908@users.noreply.github.com>
2024-10-21 19:13:34 +0530

    Delete mohnish directory

--- mohnish/PrivateLibrary - Copy/src/main/java/com/library/project/college/PrivateLibrary/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.library.project.college.PrivateLibrary.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-     public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
--- mohnish/PrivateLibrary - Copy/src/main/java/com/library/project/college/PrivateLibrary/repository/OTPRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.project.college.PrivateLibrary.repository;
-
-import com.library.project.college.PrivateLibrary.entities.OTPEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OTPRepository extends JpaRepository<OTPEntity,String> {
-}
