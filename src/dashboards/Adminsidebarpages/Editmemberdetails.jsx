e1a45c9179cd8ebabc7f853be9248af89d4e481a
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2024-11-12 22:43:26 +0530

    set template editmemberdetails

--- src/dashboards/Adminsidebarpages/Editmemberdetails.jsx
+++ src/dashboards/Adminsidebarpages/Editmemberdetails.jsx
@@ -4,7 +4,9 @@ import { useLocation } from 'react-router-dom';
 import Loader from '../../components/Loader';
 
 const Editmemberdetails = () => {
+    const location = useLocation();
     const {data} = location.state || {};
+    console.log(data);
 
   return (
     <div className='text-white'>
